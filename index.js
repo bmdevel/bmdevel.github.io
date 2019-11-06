4d23b7274419b70925f448dd668397fbd2d8664a
Robin Bochinsky <bochinsky@billiger-mietwagen.de>
2019-11-06 17:21:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+let start = document.getElementById('start');
+let list = document.getElementById('list');
+let dummy = document.getElementById('dummy');
+
+let total = settings.displaySettings.totalItems;
+let index = settings.displaySettings.winnerItemIndex;
+let items = settings.items;
+let win = total-index-1;
+
+let totalchance = 0.0;
+let windata = null;
+
+let chances = [];
+
+for(let i = 0; i<items.length; i++) {
+  for(let a = 0; a<items[i].chance; a++) chances[chances.length] = items[i];
+}
+
+async function newSpin() {
+  for(let i = 0; i<total; i++) {
+    if(i == win)
+      windata = chances[Math.floor(Math.random() * chances.length)];
+    else {
+      let e = dummy.cloneNode(true);
+      e.style.display = '';
+      e.children[0].src = chances[Math.floor(Math.random() * chances.length)].image_url;
+      list.append(e);
+    }
+  }
+}
+
+
+start.onclick = () => {
+  newSpin();
+
+  start.style.animation = 'fadeout 1s';
+  start.style.animationFillMode = 'forwards';
+
+  list.style.animation = 'fadein 1s';
+  list.style.animationFillMode = 'forwards';
+
+  setTimeout(() => {
+    list.style.opacity = '1';
+    list.style.animation = 'spin 8s ease';
+    list.style.animationFillMode = 'forwards';
+  }, 1000);
+
+  setTimeout(() => {
+    for(let i = 0; i<total-1; i++) if(i!=win) list.children[i].style.animation = "fadeout 1s forwards";
+  }, 9000);
+
+  setTimeout(() => {
+    list.children[win].style.animation = "fadeout 1s forwards";
+  }, 14000);
+  
+  setTimeout(() => {
+    list.innerHTML = '';
+    start.style.animation = "fadein 1s forwards";
+  }, 15000)
+};
---
+++ settings.js
@@ -0,0 +1,28 @@
+var settings = {
+  "displaySettings": {
+    "totalItems": 50,
+    "winnerItemIndex": 6
+  },
+  "items": [
+    {
+      "image_url": "./images/parkuhr.png",
+      "chance": 2
+    },
+    {
+      "image_url": "./images/brotdose.png",
+      "chance": 8
+    },
+    {
+      "image_url": "./images/flasche.png",
+      "chance": 14
+    },
+    {
+      "image_url": "./images/kugelschreiber.png",
+      "chance": 38
+    },
+    {
+      "image_url": "./images/flaschenoeffner.png",
+      "chance": 38
+    }
+  ]
+}
